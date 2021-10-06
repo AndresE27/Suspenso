425493f72390a056821812bc2abce7f360998005
AndresE27 <89863261+AndresE27@users.noreply.github.com>
2021-10-06 10:40:03 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,59 +1,31 @@
-from machine import Pin
-from umqtt.simple import MQTTclient
-import time 
-import webrepl
 
-B1=Pin(5, Pin.IN)
-B2=Pin(14,Pin.IN)
+from flask import Flask, render_template, request
 
-cliente = MQTTclient('andres' , 'maqiatto.com' , 1883, 'andresandino36@gmail.com' , 'Andres623Eloy')
+#import model
+import json
+import base64
+from urllib.parse import unquote
 
-def conectado(tcp,msg):
-	x=len(msg)
-	print(str(x))
-	y=len("ON")
-	z=len("OFF")
+import os
+port = int(os.environ.get("PORT", 5000))	
+PORT_NUMBER = port
 
-	if x==y:
-		cliente.publish("andresandino36@gmail.com/T2",str(a))
-    else:
-    	 if x==z:
-			cliente.publish("andresandino36@gmail.com/T2" , "S1:" +str(st))
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+@app.route("/")
+def root():
+	return render_template("index.html")
 
-cliente.set_callback(conectado)
-cliente.connect()
-cliente.subscribe("andresandino36@gmail.com/T1")
+@app.route("/index")
+def index():
+	return render_template("index.html")
 
-rc=0
-while rc==0:
-	boton1=B1.value()
-	if (boton1==0):
-		a=1
-		h=open("prue1.txt" , 'a')
-		h.write("S1:" +str(a)+";" + "S2:" +str(b)+";" + "\n" )
-		h.close()
-		h=open("prue1.txt" , 'r+')
-		tr=h.read()
-		time.sleep(3)
-		print ("sensor 1:" , +str(tr))
-		h.close()
-		time.sleep(0.3)
-	else:
-		a=0
 
-	boton2=B2.value()
-	if(boton2==0):
-		b=1
-		d=open("nombre.txt" , 'a')
-		d.write("S1:" +str(a)+ ";" + "S2:" +str(b) + ";" + "\n")
-		d.close()
-		d=open("nombre.txt" , '+r')
-		tr=d.read()
-		time.sleep(4)
-		print("S1:" +str(tr))
-		d.close()
-		time.sleep(0.3)
-	else:
-		b=0
+@app.route("/buscar")
+def buscar():
+	return render_template("buscar.html")
 
-	cliente.check_msg()
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0',port=PORT_NUMBER,debug = True)
+1
